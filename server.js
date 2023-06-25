0d6b2c2dcf7442e76fd4888c0d92c2306b08b0c1
Otso Lappalainen <otsolappalainen@gmail.com>
2023-06-25 13:56:35 +0300

    pushing this to gandi for testing

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const { execSync } = require('child_process');
+
+// Run necessary commands defined in package.json scripts
+try {
+  execSync('npm run setup', { stdio: 'inherit' });
+  execSync('npm run build', { stdio: 'inherit' });
+  execSync('npm run start', { stdio: 'inherit' });
+} catch (error) {
+  console.error('Failed to run necessary commands:', error);
+  process.exit(1);
+}
