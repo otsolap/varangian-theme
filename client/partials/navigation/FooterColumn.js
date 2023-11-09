5542cddabc9a6f7e3ecb3a127998b812af680215
Otso Lappalainen <otsolappalainen@gmail.com>
2023-11-09 21:35:45 +0200

    add image support for footer column

--- client/partials/navigation/FooterColumn.js
+++ client/partials/navigation/FooterColumn.js
@@ -4,9 +4,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons"
 import MarkdownBlock from "@/partials/util/MarkdownBlock"
 import CustomLink from "@/partials/util/CustomLink"
+import NextImage from "@/partials/util/NextImage";
 import styles from '@/styles/components/footer.module.scss'
 
-const FooterColumn = ({ id, open, title, description, links }) => {
+const FooterColumn = ({ id, open, title, description, image, links }) => {
   const contentRef = useRef(null)
   const [active, setActive] = useToggle(open)
 
@@ -42,6 +43,7 @@ const FooterColumn = ({ id, open, title, description, links }) => {
                 : `${styles.content}`
             }
           >
+            {image && <NextImage image={image} className={styles.image} />}
             {description && <MarkdownBlock markdown={description} />}
             {links ? (
             <div className={styles.linkWrapper}>
