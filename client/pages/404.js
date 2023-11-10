2efc6fc6289350f42d416e8bbea7fffcb85a8034
Otso Lappalainen <otsolappalainen@gmail.com>
2023-11-10 22:39:02 +0200

    custom 404 page done

---
+++ client/pages/404.js
@@ -0,0 +1,19 @@
+import styles from '@/styles/pages/error.module.scss'
+import Link from "next/link";
+
+export default function Custom404() {
+    return (
+        <section>
+            <header className={styles.padding}>
+                <h1>404 - Page Not Found</h1>
+            </header>
+            <article className={styles.padding}>
+                <p>Unfortunately, the page you were looking for cannot be found – it might have been moved or deleted. If you typed the address directly into the browser's address bar, you might want to double-check to make sure that no typing errors have slipped in.</p>
+                <p>Links that could help you track down the content you're looking for:</p>
+                <div className={styles.buttonWrapper}>
+                    <Link className={'button button--primary'} href="/">Home</Link>
+                </div>
+            </article>
+        </section>
+    )
+  }
