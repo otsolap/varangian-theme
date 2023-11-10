13ef1502c5ec3ac4b4e4f370d09825b8211350e5
Otso Lappalainen <otsolappalainen@gmail.com>
2023-11-10 22:46:26 +0200

    fix custom 404 page. Sigh maybe I should add strapiCMS to it. I think I should.

--- client/pages/404.js
+++ client/pages/404.js
@@ -8,8 +8,8 @@ export default function Custom404() {
                 <h1>404 - Page Not Found</h1>
             </header>
             <article className={styles.padding}>
-                <p>Unfortunately, the page you were looking for cannot be found – it might have been moved or deleted. If you typed the address directly into the browser's address bar, you might want to double-check to make sure that no typing errors have slipped in.</p>
-                <p>Links that could help you track down the content you're looking for:</p>
+                <p>Unfortunately, the page you were looking for cannot be found &ndash; it might have been moved or deleted. If you typed the address directly into the browser&apos;s address bar, you might want to double-check to make sure that no typing errors have slipped in.</p>
+                <p>Links that could help you track down the content you&apos;re looking for:</p>
                 <div className={styles.buttonWrapper}>
                     <Link className={'button button--primary'} href="/">Home</Link>
                 </div>
