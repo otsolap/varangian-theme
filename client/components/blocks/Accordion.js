84b3a3796f9f3a8d78fa307a12069e9b3949e407
Otso Lappalainen <otsolappalainen@gmail.com>
2023-12-26 10:46:43 +0200

    return null if no items on accordion block

--- client/components/blocks/Accordion.js
+++ client/components/blocks/Accordion.js
@@ -3,30 +3,31 @@ import NextImage from "@/partials/util/NextImage";
 import styles from "@/styles/components/accordion.module.scss";
 
 const Accordion = ({ title, description, image, accordions }) => {
-  return (
-    <section className={styles.accordion}>
-        {title && (
-          <header className={styles.header}>
-            {title && <h2>{title}</h2>}
-            {description && <p>{description}</p>}
-          </header>
-        )}
-      <div className={styles.wrapper}>
-          {image?.data && (
-            <figure className={styles.imageContainer}>
-              <NextImage className={styles.image} image={image} />
-            </figure>
+  return ( 
+    accordions ? (
+      <section className={styles.accordion}>
+          {title && (
+            <header className={styles.header}>
+              {title && <h2>{title}</h2>}
+              {description && <p>{description}</p>}
+            </header>
           )}
-          {accordions.data && (
-              <div className={styles.content}>
-                {accordions.data.map((item, i) => (
-                    <AccordionItem key={i} {...item.attributes} />
-                ))}
-            </div>
-          )}
-      </div>
-    </section>
-  )
+        <div className={styles.wrapper}>
+            {image?.data && (
+              <figure className={styles.imageContainer}>
+                <NextImage className={styles.image} image={image} />
+              </figure>
+            )}
+            {accordions.data && (
+                <div className={styles.content}>
+                  {accordions.data.map((item, i) => (
+                      <AccordionItem key={i} {...item.attributes} />
+                  ))}
+              </div>
+            )}
+        </div>
+      </section>
+  ) : null );
 }
 
 export default Accordion
