1c668eaa40db6d824cd32c77c483142df87968a3
Otso Lappalainen <otsolappalainen@gmail.com>
2024-01-20 07:26:58 +0200

    Favicon support added

--- client/components/util/SEO.js
+++ client/components/util/SEO.js
@@ -49,6 +49,13 @@ const SEO = ({ metadata, baseSEO, canonicalUrl }) => {
           <meta name="twitter:creator" content={`@${XTwitter}`} />
         </>
       )}
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+      <link rel="manifest" href="/icons/site.webmanifest" />
+      <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#001f2a" />
+      <meta name="msapplication-TileColor" content="#001f2a" />
+      <meta name="theme-color" content="#001f2a" />
     </Head>
   );
 };
