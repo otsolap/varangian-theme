08ec6f66f10432cf613a7ab3db95ac29b3a77eb3
Otso Lappalainen <otsolappalainen@gmail.com>
2024-01-21 09:14:08 +0200

    bring back node envs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,9 @@ module.exports = {
       args: 'run start',
       cwd: './api',
       watch: false,
+      env_production: {
+        NODE_ENV: 'production'
+      },
     },
     {
       name: 'client',
@@ -14,6 +17,9 @@ module.exports = {
       cwd: './client', 
       watch: ['./client/.next/static', './client/.next/server'],
       ignore_watch: ['./client/node_modules', './client/public', './client/.next/cache'],
+      env_production: {
+        NODE_ENV: 'production'
+      },
     },
   ],
 };
