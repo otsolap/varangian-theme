f7746ea00f50334befcef383fea3e0595c781eeb
Otso <otsolappalainen@gmail.com>
2024-02-25 20:34:11 +0200

    uid for text field too

--- client/partials/form/TextField.js
+++ client/partials/form/TextField.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import styles from '@/styles/components/form.module.css'
+import { createID } from '@/utils/index'
 
 const TextField = ({ name, placeholder, showLabels, required, formRef }) => {
+
+  const uid = createID(name)
+
   return (
     <>
-    <label htmlFor={name} className={showLabels ? '' : 'sr-only'}>{name}</label>
+    <label htmlFor={uid} className={showLabels ? '' : 'sr-only'}>{name}</label>
     <input
     type='text'
-    name={name}
-    id={name}
+    name={uid}
+    id={uid}
     placeholder={placeholder ? placeholder : name}
     required={required ? true : null}
     ref={formRef}
