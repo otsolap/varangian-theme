8b55279bd59814835c05381e22008df963e181c2
Otso <otsolappalainen@gmail.com>
2024-03-02 16:48:36 +0200

    fix subscriber

--- api/src/api/subscriber/services/subscriber.js
+++ api/src/api/subscriber/services/subscriber.js
@@ -1,27 +1,19 @@
 'use strict';
 
 /**
- * subscriber service.
+ * subscriber router
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::subscriber.subscriber', ({ strapi }) =>  ({
-  // You can add more custom functions here if needed
-
-  // Custom create function with auto-publish
-  async createAndPublish(data) {
-    try {
-      const entry = await strapi.entityService.create('api::subscriber.subscriber', { data });
-      const publishedEntry = await strapi.entityService.update('api::subscriber.subscriber', entry.id, {
-        data: { publishedAt: new Date() },
-      });
-      return publishedEntry;
-    } catch (err) {
-      throw err;
-    }
+module.exports = createCoreRouter('api::subscriber.subscriber', [
+  {
+    method: 'POST',
+    path: '/subscribers/webhook/activate',
+    handler: 'subscriber.webhookActivate',
+    config: {
+      policies: [],
+      middlewares: [],
+    },
   },
-  // ... include other default or custom service functions as needed
-}));
-
-
+]);
