5f1620eac271233d6d0aff76247152c094dbf5f2
Otso <otsolappalainen@gmail.com>
2024-03-06 21:42:43 +0200

    realized i had a bug on my convertkit solution

--- api/src/api/subscriber/services/subscriber.js
+++ api/src/api/subscriber/services/subscriber.js
@@ -6,4 +6,16 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::subscriber.subscriber');
+module.exports = createCoreService('api::subscriber.subscriber', ({ strapi }) => ({
+  async createAndPublish(data) {
+    try {
+      const entry = await strapi.entityService.create('api::subscriber.subscriber', { data });
+      const publishedEntry = await strapi.entityService.update('api::subscriber.subscriber', entry.id, {
+        data: { publishedAt: new Date() },
+      });
+      return publishedEntry;
+    } catch (err) {
+      throw err;
+    }
+  },
+}));
